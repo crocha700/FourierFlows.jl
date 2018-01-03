24ef95c0585556ad7c346fef2055097212ab712d
autodocs <autodocs>
2018-01-03 06:00:38 +0000

    build based on 4a828db

---
+++ latest/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": "#FourierFlows.jl DocumentationSome text describing the package."
+},
+
+{
+    "location": "index.html#Subtitle-1",
+    "page": "Home",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "More text"
+},
+
+{
+    "location": "index.html#Tutorials-1",
+    "page": "Home",
+    "title": "Tutorials",
+    "category": "section",
+    "text": "Pages = [\n    \"tutorials/page1.md\",\n    \"tutorials/page2.md\",\n    \"tutorials/page3.md\"\n    ]\nDepth = 2"
+},
+
+{
+    "location": "index.html#Another-Section-1",
+    "page": "Home",
+    "title": "Another Section",
+    "category": "section",
+    "text": "Pages = [\n    \"sec2/page1.md\",\n    \"sec2/page2.md\",\n    \"sec2/page3.md\"\n    ]\nDepth = 2"
+},
+
+{
+    "location": "index.html#Index-1",
+    "page": "Home",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+]}
