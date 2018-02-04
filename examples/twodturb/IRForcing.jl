79d2ad2f8e08e09d7cec8c36642abfa5c1e81dbb
Navid Constantinou <navidcy@gmail.com>
2018-02-04 13:13:27 -0800

    attempts to make a simple example with stochastic forcing with isotropic ring

--- examples/twodturb/IRForcing.jl
+++ examples/twodturb/IRForcing.jl
@@ -2,28 +2,28 @@ using PyPlot, FourierFlows
 import FourierFlows.TwoDTurb
 import FourierFlows.TwoDTurb: energy, enstrophy, dissipation, injection, drag
 
- n, L  =  128, 2π
- ν, nν = 0e-3, 1
- μ, nμ = 0e-1, 0
-dt, tf = 1e-2, 100
+ n, L  =  64, 2π
+ ν, nν = 1e-6, 2
+ μ, nμ = 1e-1, 0
+dt, tf = 0.01, 100
 
 nt = round(Int, tf/dt)
 
-nt = 1
-ns = 1
+nt = 2000
+ns = 10
 
 # Forcing
 kf, dkf = 12.0, 2.0
-σ = 0.1
+σ = 0.0001
 
 gr  = TwoDGrid(n, L)
 
-force2k = exp.(-(sqrt.(gr.Kr.^2+gr.Lr.^2)-kf).^2/(2*dkf^2))
-force2k[sqrt.(gr.Kr.^2 + gr.Lr.^2) .< 2.0 ]=0
-force2k[sqrt.(gr.Kr.^2 + gr.Lr.^2) .> 20.0 ]=0
-iksq = 1./(gr.Kr.^2 + gr.Lr.^2)
-iksq[1, 1] = 0
-σ0 = sum(force2k.*iksq/1.0)
+force2k = exp.(-(sqrt.(gr.KKrsq)-kf).^2/(2*dkf^2))
+force2k[gr.KKrsq .< 2.0^2 ] = 0
+force2k[gr.KKrsq .> 20.0^2 ] = 0
+force2k[gr.Kr.<2π/L] = 0
+# σ0 = sum(force2k.*gr.invKKrsq/2.0)
+σ0 = FourierFlows.parsevalsum(force2k.*gr.invKKrsq/2.0, gr)/(g.Lx*g.Ly)
 force2k .= σ/σ0 * force2k
 # if size(force2k)[1]==g.nkr
 #     force2k .= 2*force2k
@@ -35,15 +35,14 @@ srand(1234)
 function calcF!(F, sol, t, s, v, p, g)
   if t == s.t # not a substep
     # eta = (randn(size(sol)) + im*randn(size(sol)))/(sqrt(2)*sqrt(s.dt))
-    # F .= eta.*sqrt.(force2k)*(g.nx*g.ny)
-    F .= 0.0
-    F[2, 3] .= 1
+    eta = exp.(2π*im*rand(size(sol)))/sqrt(s.dt)
+    @. F = eta * sqrt(force2k)
   end
   nothing
 end
 
 prob = TwoDTurb.ForcedProblem(nx=n, Lx=L, ν=ν, nν=nν, μ=μ, nμ=nμ, dt=dt,
-  stepper="ForwardEuler", calcF=calcF!)
+  stepper="RK4", calcF=calcF!)
 
 s, v, p, g, eq, ts = prob.state, prob.vars, prob.params, prob.grid, prob.eqn, prob.ts;
 
@@ -79,7 +78,7 @@ function makeplot(prob, diags)
   ii = (i₀+1):E.count
 
   # dEdt = I - D - R?
-  total = I[ii] - D[ii] - R[ii]
+  total = σ/2+I[ii] - D[ii] - R[ii]
   residual = dEdt - total
 
   # plot(E.time[ii], I[ii], "o", markersize=0.5, label="injection (\$I\$)")
@@ -87,26 +86,33 @@ function makeplot(prob, diags)
   # plot(E.time[ii], -R[ii], label="drag (\$R\$)")
   # plot(E.time[ii], residual, "c-", label="residual")
   # plot(E.time[ii], dEdt[ii], label="dissipation (\$dE/dt\$)")
-  plot(E.time[ii], I[ii], label="injection (\$I\$)")
-  # plot(E.time[ii], -D[ii], label="dissipation (\$D\$)")
-  # plot(E.time[ii], -R[ii], label="drag (\$R\$)")
-  # plot(E.time[ii], residual, "c-", label="residual")
-
+  plot(E.time[ii], I[ii] + σ/2, label="injection (\$I\$)")
+  plot(E.time[ii], -D[ii], label="dissipation (\$D\$)")
+  plot(E.time[ii], -R[ii], label="drag (\$R\$)")
   ylabel("Energy sources and sinks")
   xlabel(L"t")
   legend(fontsize=10)
 
   sca(axs[3]); cla()
+  plot(E.time[ii], residual, "c-", label="residual")
+
+  ylabel("Energy sources and sinks")
+  xlabel(L"t")
+  legend(fontsize=10)
+
+  sca(axs[4]); cla()
   plot(E.time[ii], E[ii])
   # plot(E.time[ii], 0.1*E.time[ii], label="predicted (\$E\$)")
   xlabel(L"t")
   ylabel(L"E")
 
+  println(mean(I[ii]))
+
   residual
 end
 
 
-# fig, axs = subplots(ncols=3, nrows=1, figsize=(13, 4))
+fig, axs = subplots(ncols=2, nrows=2, figsize=(10, 8))
 
 # Step forward
 for i = 1:ns
@@ -120,8 +126,8 @@ for i = 1:ns
   cfl = prob.ts.dt*maximum(
     [maximum(prob.vars.V)/prob.grid.dx, maximum(prob.vars.U)/prob.grid.dy])
 
-  # res = makeplot(prob, diags)
-  # pause(0.1)
+  res = makeplot(prob, diags)
+  pause(0.1)
 
   # @printf("step: %04d, t: %.1f, cfl: %.3f, time: %.2f s, mean(res) = %.3e\n",
   #   prob.step, prob.t, cfl, tc, mean(res))
